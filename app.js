29354a694840ce180c27797724843a93be048b86
Anthony Beck <anthonydbeck77@gmail.com>
2021-03-08 23:04:47 +1000

    sass refactor, more page content.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 function toggleMobileMenu() {
-    document.getElementById("header-navbar").classList.toggle('navbar-visibility');
+    // Toggle the "navbar-toggle" class on the header navbar element with css id "header-navbar".
+    document.getElementById("header-navbar").classList.toggle('navbar-toggle');
 }
